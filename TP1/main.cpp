536e8a6eff52cfe2fae09db03960b3f29b0ea59e
Bárbara Martins <martins.barbara04@gmail.com>
2022-10-11 07:53:56 -0300

    Init project

---
+++ TP1/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+int main() {
+  
+  //make a vector of ints
+  std::vector<int> v;
+
+  //make a map of strings to ints
+  std::map<std::string, int> m;
+
+  // call a function in another file
+
+  return(0);
+}
